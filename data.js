bb1a6c465fdbe7d2533e4d36ab95f73942fac2b6
Maximogargiulo11 <maximogargiulo0@gmail.com>
2026-06-24 21:40:40 -0300

    admin: actualizar datos del sitio

--- data.js
+++ data.js
@@ -545,6 +545,58 @@ window.BAG_DATA = {
           "peso": "186 gr.",
           "coleccion": "‘Attack’ Pack"
         }
+      },
+      {
+        "id": "d4s9li3",
+        "name": "Nike Mercurial Vapor 17 Elite FG",
+        "colorway": "Negro / Verde",
+        "color": "#000000",
+        "price": 549999,
+        "availableSizes": [],
+        "sizes": {
+          "eu": [
+            "38",
+            "38.5",
+            "39",
+            "39.5",
+            "40",
+            "40.5",
+            "41",
+            "42",
+            "42.5",
+            "43",
+            "44",
+            "44.5",
+            "45",
+            "46"
+          ],
+          "us": [
+            "7",
+            "7.5",
+            "8",
+            "8.5",
+            "9",
+            "9.5",
+            "10",
+            "10.5",
+            "11",
+            "11.5",
+            "12",
+            "12.5",
+            "13"
+          ],
+          "uk": []
+        },
+        "images": [
+          "assets/1782348005757-IMG_2186.webp"
+        ],
+        "videos": [],
+        "spec": {
+          "suela": "FG ",
+          "terreno": "Césped natural firme",
+          "peso": "186 gr.",
+          "coleccion": "‘Shadow’ Pack (actualizado)"
+        }
       }
     ],
     "nike/phantom": [
