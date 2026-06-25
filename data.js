9fcd36a0c4cc7355bfe9c62ae4138a7f4782f001
Maximogargiulo11 <maximogargiulo0@gmail.com>
2026-06-24 22:01:40 -0300

    admin: actualizar datos del sitio

--- data.js
+++ data.js
@@ -1052,6 +1052,58 @@ window.BAG_DATA = {
           "peso": "186 gr.",
           "coleccion": "‘Shadow’ Pack (actualizado)"
         }
+      },
+      {
+        "id": "8dv9hyf",
+        "name": "Nike Phantom 6 High Elite SG-Pro Player Edition ",
+        "colorway": "Negro / Verde",
+        "color": "#000000",
+        "price": 699999,
+        "availableSizes": [],
+        "sizes": {
+          "eu": [
+            "38",
+            "38.5",
+            "39",
+            "39.5",
+            "40",
+            "40.5",
+            "41",
+            "42",
+            "42.5",
+            "43",
+            "44",
+            "44.5",
+            "45",
+            "46"
+          ],
+          "us": [
+            "7",
+            "7.5",
+            "8",
+            "8.5",
+            "9",
+            "9.5",
+            "10",
+            "10.5",
+            "11",
+            "11.5",
+            "12",
+            "12.5",
+            "13"
+          ],
+          "uk": []
+        },
+        "images": [
+          "assets/1782348717767-IMG_2190.webp"
+        ],
+        "videos": [],
+        "spec": {
+          "suela": "SG",
+          "terreno": "Césped natural blando",
+          "peso": "186 gr.",
+          "coleccion": "‘Shadow’ Pack (actualizado)"
+        }
       }
     ],
     "nike/tiempo": [
