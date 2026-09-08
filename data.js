dfaf0f8694f01d999841a75fb194c00c02236440
Maximogargiulo11 <maximogargiulo0@gmail.com>
2026-09-08 12:02:23 -0300

    admin: actualizar datos del sitio

--- data.js
+++ data.js
@@ -6340,6 +6340,56 @@ window.BAG_DATA = {
           "peso": "186 gr.",
           "coleccion": "‘Inmortal DNA’ Pack"
         }
+      },
+      {
+        "id": "19ki87l",
+        "name": "Adidas Predator Elite FT FG SE Zinedine Zidane 30 ",
+        "colorway": "Blanco ",
+        "color": "#ffffff",
+        "price": 619999,
+        "availableSizes": [],
+        "sizes": {
+          "eu": [
+            "38",
+            "38.5",
+            "39",
+            "39.5",
+            "40",
+            "40.5",
+            "41",
+            "42",
+            "42.5",
+            "43",
+            "44",
+            "44.5",
+            "45",
+            "46"
+          ],
+          "us": [
+            "7",
+            "7.5",
+            "8",
+            "8.5",
+            "9",
+            "9.5",
+            "10",
+            "10.5",
+            "11",
+            "11.5",
+            "12"
+          ],
+          "uk": []
+        },
+        "images": [
+          "assets/1788879702296-IMG_3766.webp"
+        ],
+        "videos": [],
+        "spec": {
+          "suela": "FG",
+          "terreno": "Césped natural firme",
+          "peso": "186 gr.",
+          "coleccion": "ZZ30"
+        }
       }
     ],
     "adidas/copa": [
